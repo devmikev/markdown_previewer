cfecfabe3fd0d2f0b20c8498c55aa67e16ec24c5
Mike V <dev.mikev@gmail.com>
2018-05-02 13:29:59 -0400

    test markdown.js functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,7 @@
-alert("CONNECTED");
+console.log("connected");
+
+var editorInput = document.querySelector("#editor");
+editorInput.addEventListener("keyup", () => {
+  document.getElementById('preview').innerHTML =
+  marked(editorInput.value);
+});

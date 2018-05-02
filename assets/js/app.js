4693c8a69e211c47a543e0357fe80e5af23becfe
Mike V <dev.mikev@gmail.com>
2018-05-02 14:30:33 -0400

    complete story 5 requirements

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,12 @@
-console.log("connected");
+var input = document.querySelector("#editor");
 
-var editorInput = document.querySelector("#editor");
-editorInput.addEventListener("keyup", () => {
+input.addEventListener("keyup", () => {
   document.getElementById('preview').innerHTML =
-  marked(editorInput.value);
+  marked(input.value);
+});
+
+$(document).ready(() => {
+  input.value = '# H1 Element\n## H2 Element\nHere\'s a link to [GitHub](https://github.com)\n\nSome in-line code: `<p></p>` \n\n ```\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == "```" && lastLine == "```") {\n    return multiLineCode;\n  }\n}```\n > Block Quotes!\n - List item: \n - some more \n - and more \n\n **bold text** \n ![Logo w/ Text](https://bit.ly/2rfDhUl)'
+  document.getElementById('preview').innerHTML =
+  marked(input.value);
 });
